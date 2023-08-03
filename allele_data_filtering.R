fdf42969a5f1014d524150cb3ed0514f92abf3af
manuelgug <36465310+manuelgug@users.noreply.github.com>
2023-08-03 18:27:23 +0200

    Add --use_case functionality
    Input a list of amplicons of interest. The script will check if there are false positives of those amplicons at each filtering stage. An additional row  and a plot of the amount of use case amplicons are added to the report and figure, respectively.

--- allele_data_filtering.R
+++ allele_data_filtering.R
@@ -10,7 +10,8 @@ option_list <- list(
   make_option(c("--resmarkers_table"), type = "character", default = NULL, help = "Path to the resmarkers table"),
   make_option(c("--CFilteringMethod"), type = "character", default = "global_max", help = "Contaminants filtering method: global_max, global_q95, amp_max, amp_q95"),
   make_option(c("--MAF"), type = "numeric", default = 0, help = "Minimum allele frequency; default 0"),
-  make_option(c("--exclude_file"), type = "character", default = NULL, help = "Path to the file containing sampleIDs to exclude")
+  make_option(c("--exclude_file"), type = "character", default = NULL, help = "Path to the file containing sampleIDs to exclude"),
+  make_option(c("--use_case_amps"), type = "character", default = NULL, help = "Path to the file amplicons of your use case")
 )
 
 opt_parser <- OptionParser(option_list = option_list)
@@ -21,6 +22,7 @@ resmarkers_table <- opt$resmarkers_table
 CFilteringMethod <- opt$CFilteringMethod
 MAF <- opt$MAF
 exclude_file<- opt$exclude_file
+use_case_amps<- opt$use_case_amps
 
 #import allele table
 allele.data<-read.csv(allele_table, sep ="\t")
@@ -75,26 +77,26 @@ pos_controls_before_index <- grepl("(?i)3D7", allele.data$sampleID) & !grepl("(?
 pos_controls_before <- allele.data[pos_controls_before_index, ]
 multiple_alleles_before<-pos_controls_before[pos_controls_before$n.alleles > 1,] # all alleles from 3D7 samples must be 1 (single copy), more than that can be considered false positives
 
-false_positives_before <- suppressWarnings({
+false_positives_initial <- suppressWarnings({
   multiple_alleles_before %>%
     group_by(sampleID, locus) %>%
     filter(norm.reads.locus != max(norm.reads.locus))}) # remove the most frequent allele of each amplicon from each sample and keep the others as false positives for filtering
 
-#print(paste("There were", dim(allele.data)[1]-dim(false_positives_before)[1], "alleles and", dim(false_positives_before)[1], "false positive alleles across", length(unique(false_positives_before$locus)), "amplicons from", length(unique(pos_controls_before$sampleID)), "`3D7 (monoclonal, single copy)` positive controls before applying any filter"))
+#print(paste("There were", dim(allele.data)[1]-dim(false_positives_initial)[1], "alleles and", dim(false_positives_initial)[1], "false positive alleles across", length(unique(false_positives_initial$locus)), "amplicons from", length(unique(pos_controls_before$sampleID)), "`3D7 (monoclonal, single copy)` positive controls before applying any filter"))
 
-initial_alleles<-dim(allele.data)[1]-dim(false_positives_before)[1]
-initial_false_positives_div<-sum(false_positives_before$Category == "Diversity")
-initial_false_positives_res<-sum(false_positives_before$Category == "Resistance")
-initial_false_positives_imm<-sum(false_positives_before$Category == "Immune")
-initial_false_positives_diag<-sum(false_positives_before$Category == "Diagnostic")
-initial_amplicons_w_false_positives<-length(unique(false_positives_before$locus))
-initial_controls_w_false_positives<-length(unique(false_positives_before$sampleID))
+initial_alleles<-dim(allele.data)[1]-dim(false_positives_initial)[1]
+initial_false_positives_div<-sum(false_positives_initial$Category == "Diversity")
+initial_false_positives_res<-sum(false_positives_initial$Category == "Resistance")
+initial_false_positives_imm<-sum(false_positives_initial$Category == "Immune")
+initial_false_positives_diag<-sum(false_positives_initial$Category == "Diagnostic")
+initial_amplicons_w_false_positives<-length(unique(false_positives_initial$locus))
+initial_controls_w_false_positives<-length(unique(false_positives_initial$sampleID))
 
-initial_alleles_positive<-unique(false_positives_before$asv)
+initial_alleles_positive<-unique(false_positives_initial$asv)
 initial_alleles_negative<-unique(neg_controls$asv)
 initial_shared_contaminants_alleles<-length(intersect(initial_alleles_positive, initial_alleles_negative))
 
-initial_amps_positive<-unique(false_positives_before$locus)
+initial_amps_positive<-unique(false_positives_initial$locus)
 initial_amps_negative<-unique(neg_controls$locus)
 initial_shared_contaminants_amps<-length(intersect(initial_amps_positive, initial_amps_negative))
 
@@ -137,26 +139,26 @@ pos_controls_index <- grepl("(?i)3D7", filtered_allele.data$sampleID) & !grepl("
 pos_controls <- filtered_allele.data[pos_controls_index, ]
 multiple_alleles<-pos_controls[pos_controls$n.alleles > 1,] # all alleles from 3D7 samples must be 1 (single copy), more than that can be considered false positives
 
-false_positives <- suppressWarnings({
+false_positives_1 <- suppressWarnings({
   multiple_alleles %>%
     group_by(sampleID, locus) %>%
     filter(norm.reads.locus != max(norm.reads.locus))}) # remove the most frequent allele of each amplicon from each sample and keep the others as false positives for filtering
 
-#print(paste("There were", dim(filtered_allele.data)[1]-dim(false_positives)[1], "alleles and", dim(false_positives)[1], "false positive alleles across", length(unique(false_positives$locus)), "amplicons from", length(unique(false_positives$sampleID)), "`3D7 (monoclonal, single copy)` positive controls after applying the contaminants filter"))
+#print(paste("There were", dim(filtered_allele.data)[1]-dim(false_positives_1)[1], "alleles and", dim(false_positives_1)[1], "false positive alleles across", length(unique(false_positives_1$locus)), "amplicons from", length(unique(false_positives_1$sampleID)), "`3D7 (monoclonal, single copy)` positive controls after applying the contaminants filter"))
 
-contaminants_filter_alleles<-dim(filtered_allele.data)[1]-dim(false_positives)[1]
-contaminants_filter_false_positives_div<-sum(false_positives$Category == "Diversity")
-contaminants_filter_false_positives_res<-sum(false_positives$Category == "Resistance")
-contaminants_filter_false_positives_imm<-sum(false_positives$Category == "Immune")
-contaminants_filter_false_positives_diag<-sum(false_positives$Category == "Diagnostic")
-contaminants_filter_amplicons_w_false_positives<-length(unique(false_positives$locus))
-contaminants_filter_controls_w_false_positives<-length(unique(false_positives$sampleID))
+contaminants_filter_alleles<-dim(filtered_allele.data)[1]-dim(false_positives_1)[1]
+contaminants_filter_false_positives_div<-sum(false_positives_1$Category == "Diversity")
+contaminants_filter_false_positives_res<-sum(false_positives_1$Category == "Resistance")
+contaminants_filter_false_positives_imm<-sum(false_positives_1$Category == "Immune")
+contaminants_filter_false_positives_diag<-sum(false_positives_1$Category == "Diagnostic")
+contaminants_filter_amplicons_w_false_positives<-length(unique(false_positives_1$locus))
+contaminants_filter_controls_w_false_positives<-length(unique(false_positives_1$sampleID))
 
-contaminants_filter_alleles_positive<-unique(false_positives$asv)
+contaminants_filter_alleles_positive<-unique(false_positives_1$asv)
 contaminants_filter_alleles_negative<-unique(neg_controls$asv)
 contaminants_filter_shared_contaminants_alleles<-length(intersect(contaminants_filter_alleles_positive, contaminants_filter_alleles_negative))
 
-contaminants_filter_amps_positive<-unique(false_positives$locus)
+contaminants_filter_amps_positive<-unique(false_positives_1$locus)
 contaminants_filter_amps_negative<-unique(neg_controls$locus)
 contaminants_filter_shared_contaminants_amps<-length(intersect(contaminants_filter_amps_positive, contaminants_filter_amps_negative))
 
@@ -178,26 +180,26 @@ pos_controls_index <- grepl("(?i)3D7", filtered_allele.data$sampleID) & !grepl("
 pos_controls <- filtered_allele.data[pos_controls_index, ]
 multiple_alleles<-pos_controls[pos_controls$n.alleles > 1,] # all alleles from 3D7 samples must be 1 (single copy), more than that can be considered false positives
 
-false_positives <- suppressWarnings({
+false_positives_2 <- suppressWarnings({
   multiple_alleles %>%
     group_by(sampleID, locus) %>%
     filter(norm.reads.locus != max(norm.reads.locus))}) # remove the most frequent allele of each amplicon from each sample and keep the others as false positives for filtering
 
-#print(paste("There are", dim(filtered_allele.data)[1]-dim(false_positives)[1], "alleles and", dim(false_positives)[1], "false positive alleles across", length(unique(false_positives$locus)), "amplicons from", length(unique(false_positives$sampleID)), "`3D7 (monoclonal, single copy)` positive controls after applying the contaminats and MAF filters"))
+#print(paste("There are", dim(filtered_allele.data)[1]-dim(false_positives_2)[1], "alleles and", dim(false_positives_2)[1], "false positive alleles across", length(unique(false_positives_2$locus)), "amplicons from", length(unique(false_positives_2$sampleID)), "`3D7 (monoclonal, single copy)` positive controls after applying the contaminats and MAF filters"))
 
-frequency_filter_alleles<-dim(filtered_allele.data)[1]-dim(false_positives)[1]
-frequency_filter_false_positives_div<-sum(false_positives$Category == "Diversity")
-frequency_filter_false_positives_res<-sum(false_positives$Category == "Resistance")
-frequency_filter_false_positives_imm<-sum(false_positives$Category == "Immune")
-frequency_filter_false_positives_diag<-sum(false_positives$Category == "Diagnostic")
-frequency_filter_amplicons_w_false_positives<-length(unique(false_positives$locus))
-frequency_filter_controls_w_false_positives<-length(unique(false_positives$sampleID))
+frequency_filter_alleles<-dim(filtered_allele.data)[1]-dim(false_positives_2)[1]
+frequency_filter_false_positives_div<-sum(false_positives_2$Category == "Diversity")
+frequency_filter_false_positives_res<-sum(false_positives_2$Category == "Resistance")
+frequency_filter_false_positives_imm<-sum(false_positives_2$Category == "Immune")
+frequency_filter_false_positives_diag<-sum(false_positives_2$Category == "Diagnostic")
+frequency_filter_amplicons_w_false_positives<-length(unique(false_positives_2$locus))
+frequency_filter_controls_w_false_positives<-length(unique(false_positives_2$sampleID))
 
-frequency_filter_alleles_positive<-unique(false_positives$asv)
+frequency_filter_alleles_positive<-unique(false_positives_2$asv)
 frequency_filter_alleles_negative<-unique(neg_controls$asv)
 frequency_filter_shared_contaminants_alleles<-length(intersect(frequency_filter_alleles_positive, frequency_filter_alleles_negative))
 
-frequency_filter_amps_positive<-unique(false_positives$locus)
+frequency_filter_amps_positive<-unique(false_positives_2$locus)
 frequency_filter_amps_negative<-unique(neg_controls$locus)
 frequency_filter_shared_contaminants_amps<-length(intersect(frequency_filter_amps_positive, frequency_filter_amps_negative))
 
@@ -229,15 +231,13 @@ colnames(report)[1] <- ""
 colnames(report)[3]<-paste0("contaminants_filter_", CFilteringMethod_)
 colnames(report)[4]<-paste0("frequency_filter_", MAF)
 
-#### EXPORTS ####
-
 base_filename <- basename(allele_table)
 filename <- tools::file_path_sans_ext(base_filename)
 
 filtered_allele.data <- filtered_allele.data[, c(2, 1, 3:ncol(filtered_allele.data))]
 
 write.table(filtered_allele.data,file=paste0(filename, "_", CFilteringMethod_, "_", as.character(MAF), "_filtered.csv"),quote=F,sep="\t",col.names=T,row.names=F)
-write.table(report,file=paste0(filename, "_", CFilteringMethod_, "_", as.character(MAF), "_filter_report.csv"),quote=F,sep=",",col.names=T,row.names=F)
+
 
 
 ## VISUALIZATION ##
@@ -260,7 +260,8 @@ plot_total <- ggplot(plot_data[plot_data$alleles == "total_alleles", ], aes(x = 
        x = "",
        y = "Total Alleles") +
   theme_minimal() +
-  scale_y_continuous(limits = c(0, max(plot_data$Value) * 1.02))  # Set the lower limit to 0
+  scale_y_continuous(limits = c(0, max(plot_data$Value) * 1.02))+  # Set the lower limit to 0
+  theme(axis.text.x = element_text(angle = -10, hjust = 0.33))
 
 plot_rest <- ggplot(plot_data[plot_data$alleles != "total_alleles", ], aes(x = Category, y = Value, group = alleles, color = alleles)) +
   geom_line(linewidth = 1.5) +
@@ -269,11 +270,8 @@ plot_rest <- ggplot(plot_data[plot_data$alleles != "total_alleles", ], aes(x = C
        y = "False Positive Alleles") +
   theme_minimal()+
   scale_y_continuous(limits = c(0, max(plot_data[plot_data$alleles != "total_alleles", ][,3]) + 1)) +
-  theme(legend.title=element_blank())
-
-fig<-grid.arrange(plot_total, plot_rest, ncol = 2)
-
-ggsave(paste0(filename, "_", CFilteringMethod_, "_", as.character(MAF), "_filter_report.png"), fig, width = 14, height = 7, dpi = 300)
+  theme(legend.title=element_blank())+
+  theme(axis.text.x = element_text(angle = -10, hjust = 0.33))
 
 
 ### MICROHAPS FILTERING ###
@@ -329,4 +327,59 @@ if (!is.null(resmarkers_table)){
   filename2 <- tools::file_path_sans_ext(base_filename2)
   
   write.table(microhaps_filtered,file=paste0(filename2, "_", CFilteringMethod_, "_", as.character(MAF), "_filtered.csv"),quote=F,sep="\t",col.names=T,row.names=F)
+}
+
+
+### Specific use case report Are there amplicons from use_case on false_positives_initial, false_positives_1, false_positives_2?
+
+if (!is.null(use_case_amps)){
+  uc_amps <- read.csv("use_case.csv") #cambiar por use_case_amps!!!
+  
+  use_case_false_positives_initial <- sum(uc_amps$locus.pool %in% false_positives_initial$sampleID)
+  use_case_false_positives_1 <- sum(uc_amps$locus.pool %in% false_positives_1$sampleID)
+  use_case_false_positives_2 <- sum(uc_amps$locus.pool %in% false_positives_2$sampleID)
+  
+  use_case_row<-c("false_positives_use_case_amplicons", use_case_false_positives_initial, use_case_false_positives_1, use_case_false_positives_2)
+  use_case_report <- rbind(report, use_case_row)
+  rownames(use_case_report)[9]<-"false_positives_use_case_amplicons"
+  
+  #report with use case
+  write.table(use_case_report,file=paste0(filename, "_", CFilteringMethod_, "_", as.character(MAF), "_filter_USE_CASE_report.csv"),quote=F,sep=",",col.names=T,row.names=F)
+  
+  use_case_report_2 <- use_case_report[9,]
+  colnames(use_case_report_2)[1]<-"amps"
+  use_case_report_2$amps<-c("use_case_amps")
+  
+  plot_data_2 <- gather(use_case_report_2, key = "Category", value = "Value", -amps)
+  
+  # Define the desired order of columns for the x-axis
+  desired_order <- c(unique(plot_data_2$Category)[1], unique(plot_data_2$Category)[2], unique(plot_data_2$Category)[3])
+  
+  # Reorder the rows based on the desired order of columns
+  plot_data_2$Category <- factor(plot_data_2$Category, levels = desired_order)
+  plot_data_2<-plot_data_2[-4,]
+  
+  plot_use_case <- ggplot(plot_data_2, aes(x = Category, y = as.numeric(Value), group = amps, color = amps)) +
+    geom_line(linewidth = 1.5) +
+    labs(title = "",
+         x = "",
+         y = "False Positive Amplicons") +
+    theme_minimal()+
+    scale_y_continuous(limits = c(0, max(as.numeric(plot_data_2[,3])) + 1)) +
+    theme(legend.title=element_blank())+
+    theme(axis.text.x = element_text(angle = -10, hjust = 0.33))
+  
+  #plot with use case
+  fig<-grid.arrange(plot_total, plot_rest, plot_use_case, ncol = 3)
+  ggsave(paste0(filename, "_", CFilteringMethod_, "_", as.character(MAF), "_filter_USE_CASE_report.png"), fig, width = 21, height = 10.5, dpi = 300)
+  
+}else{
+  #### EXPORTS WITHOUT USE CASE####
+  
+  #report
+  write.table(report,file=paste0(filename, "_", CFilteringMethod_, "_", as.character(MAF), "_filter_report.csv"),quote=F,sep=",",col.names=T,row.names=F)
+  
+  #plot
+  fig<-grid.arrange(plot_total, plot_rest, ncol = 2)
+  ggsave(paste0(filename, "_", CFilteringMethod_, "_", as.character(MAF), "_filter_report.png"), fig, width = 14, height = 7, dpi = 300)
 }
